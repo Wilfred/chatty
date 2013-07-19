9b78251fe57ff1b28d81cf4d99b5678e862b5f7a
Wilfred Hughes <me@wilfred.me.uk>
2013-07-19 18:14:12 +0100

    set buffering and write to socket

--- src/Irc.hs
+++ src/Irc.hs
@@ -1,4 +1,5 @@
 import Network(connectTo, PortNumber, PortID)
+import System.IO(hSetBuffering, BufferMode(NoBuffering), hPutStr)
 
 data IrcServer = IrcServer {
   address :: String,
@@ -16,6 +17,10 @@ sayMessage message = do
   let server' = (server message)
       address' = (address server')
       port' = (port server')
-  h <- connectTo address' port'
+  socket <- connectTo address' port'
+  hSetBuffering socket NoBuffering
+  -- TODO: send the necessary hello first
+  -- TODO: factor out a function that writes valid IRC message syntax
+  hPutStr socket "todo"
   return ()
   
