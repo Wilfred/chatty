939aaf271f97f774c002a0d9c27c3a263c8d8060
Wilfred Hughes <me@wilfred.me.uk>
2013-07-19 18:02:58 +0100

    basic sayMessage skeleton

--- src/Irc.hs
+++ src/Irc.hs
@@ -1,12 +1,21 @@
-import System.IO
+import Network(connectTo, PortNumber, PortID)
 
 data IrcServer = IrcServer {
   address :: String,
-  port :: Int
+  port :: PortID
   }
 
 data IrcMessage = IrcMessage {
   server :: IrcServer,
   channel :: String,
-  content :: String,
+  content :: String
   }
+
+sayMessage :: IrcMessage -> IO ()
+sayMessage message = do
+  let server' = (server message)
+      address' = (address server')
+      port' = (port server')
+  h <- connectTo address' port'
+  return ()
+  
