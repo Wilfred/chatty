de6c60097aa2588d2edad1c00298f09260625c14
Wilfred Hughes <me@wilfred.me.uk>
2013-09-19 12:15:44 +0100

    read the port passed in

--- src/Main.hs
+++ src/Main.hs
@@ -8,6 +8,7 @@ import           Snap.Http.Server
 
 import Control.Monad.IO.Class (liftIO)
 
+import Data.Maybe(listToMaybe, fromMaybe)
 import Data.ByteString.Char8(unpack)
 import Network(PortID(PortNumber))
 import Irc
@@ -27,6 +28,12 @@ sendHandler = do
   channel <- getParam "channel"
   message <- getParam "message"
 
+  -- allow the user to specify a port, but defualt to 6667 if not given or invalid number.
+  port <- getParam "port"
+  let portNumber = case port of
+        Just port' -> fromMaybe 6667 (maybeRead (unpack port') :: Maybe Integer)
+        _ -> 6667
+
   -- allow the user to specify a nick, but default to 'chatty'
   nick <- getParam "nick"
   let ircNick = maybe (IrcNick "chatty") (\n -> (IrcNick (unpack n))) nick
@@ -39,3 +46,7 @@ sendHandler = do
       liftIO $ sendToChannel ircServer ircNick ircMessage
       writeBS "ok"
     _ -> writeBS "Need a server, a port, a channel and a message."
+
+
+maybeRead :: Read a => String -> Maybe a
+maybeRead = fmap fst . listToMaybe . reads
